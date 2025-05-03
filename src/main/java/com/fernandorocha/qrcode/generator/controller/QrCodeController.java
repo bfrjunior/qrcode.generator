d5a6206eeece70238ed4be1daf6f2c97099c84fb
Fernando Rocha <beneditofernandor@gmail.com>
2025-05-03 14:50:46 -0300

    feat: class Qrcode

--- src/main/java/com/fernandorocha/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/fernandorocha/qrcode/generator/controller/QrCodeController.java
@@ -1,5 +1,22 @@
 package com.fernandorocha.qrcode.generator.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fernandorocha.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.fernandorocha.qrcode.generator.dto.QrCodeGenerateResponse;
+
+@RestController
+@RequestMapping("/qrcode")
 public class QrCodeController {
     
+    @PostMapping
+     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
+    
+        return null;
+    }
 }
